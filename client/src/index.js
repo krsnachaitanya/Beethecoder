9d726041f590a2f40eaef64c81defd4088348ad6
Chaitu <chaitu.ca539@gmail.com>
2021-05-09 18:20:24 +0530

    add tailwindcss to use in styled components and config files

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+import '../src/assets/tailwind.css';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
