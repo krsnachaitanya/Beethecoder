714dee9d379a060a77859e6673b42da6dd93797a
Chaitu <chaitu.ca539@gmail.com>
2021-05-10 13:23:13 +0530

    Adding styling to navbar

--- client/src/core/NavBar.js
+++ client/src/core/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import tw from 'twin.macro';
@@ -8,43 +9,47 @@ const NavBarStyles = styled.nav`
   ${tw`bg-gray-800`}
   & {
     ul {
-      ${tw`list-none`}
+      ${tw`flex gap-8 list-none`}
     }
   }
 `;
 
 const NavLink = styled(Link)`
-  ${tw`text-yellow-100 no-underline`}
+  ${tw`text-white no-underline hover:text-green-500`}
+  ${(props) => props.isactive && tw`text-green-500`}
 `;
 
-const NavBar = () => {
+const navigation = [
+  { name: 'Home', href: '/' },
+  { name: 'Cart', href: '/cart' },
+  { name: 'Dashboard', href: '/dashboard' },
+  { name: 'Admin Dashboard', href: '/admin/dashboard' },
+  { name: 'Signup', href: '/signup' },
+  { name: 'Signin', href: '/signin' },
+  { name: 'Signout', href: '/singout' },
+];
+
+const NavBar = ({ history }) => {
   return (
     <NavBarStyles as="nav">
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Cart</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Dashboard</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Admin Dashboard</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Signup</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Sign In</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Sign Out</NavLink>
-        </li>
+        {navigation.map((link, index) => (
+          <li key={index}>
+            <NavLink
+              to={link.href}
+              isactive={history.location.pathname === link.href ? 1 : undefined}
+            >
+              {link.name}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </NavBarStyles>
   );
+};
+
+NavBar.propTypes = {
+  history: PropTypes.object,
 };
 
 export default withRouter(NavBar);
