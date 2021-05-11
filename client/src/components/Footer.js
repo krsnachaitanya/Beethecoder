d9304a78a83910ac0e190313c0ad2047e9365336
Chaitu <chaitu.ca539@gmail.com>
2021-05-11 07:32:58 +0530

    footer design and file restructure

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const FooterStyles = styled.footer`
+  ${tw`p-6 text-center bg-gray-900`}
+`;
 
 const Footer = () => {
   return (
-    <footer>
+    <FooterStyles>
       <div>
         <h4>If you got any questions, feel free to reach out.</h4>
         <button>Contact Us</button>
       </div>
-      <div>
-        <span></span>
-      </div>
-    </footer>
+    </FooterStyles>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import '../src/assets/tailwind.css';
-import GlobalStyles from './styles/GlobalStyles';
+import GlobalStyles from './components/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
